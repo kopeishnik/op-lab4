95da2eb6bcd76743c0df9a73c430714a74dc3cbd
Andrew <71765448+CherpakAndrii@users.noreply.github.com>
2021-04-19 22:00:17 +0300

    Тепер воно працює, але не повністю)
    обрізає кілька рядків пікселів(

--- Pict.cs
+++ Pict.cs
@@ -13,8 +13,11 @@ namespace bmpTest
         private Int16 _pictH16;
         private Int32 _pictW32;
         private Int32 _pictH32;
+        private int _realH;
+        private int _realW;
         private byte[] lastBites;
         private byte[][][] _data;
+        private byte nullb;
 
         public Pict(string path)
         {
@@ -25,15 +28,15 @@ namespace bmpTest
             bcSize = br.ReadInt32();
             if (bcSize == 12)
             {
-                _pictW16 = br.ReadInt16();
-                _pictH16 = br.ReadInt16();
+                _realW = _pictW16 = br.ReadInt16();
+                _realH = _pictH16 = br.ReadInt16();
                 _pictW32 = _pictH32 = 0;
                 lastBites = br.ReadBytes(4);
             }
             else if (bcSize == 40)
             {
-                _pictW32 = br.ReadInt32();
-                _pictH32 = br.ReadInt32();
+                _realW = _pictW32 = br.ReadInt32();
+                _realH = _pictH32 = br.ReadInt32();
                 _pictW16 = _pictH16 = 0;
                 lastBites = br.ReadBytes(28);
             }
@@ -44,16 +47,20 @@ namespace bmpTest
                     Console.WriteLine(bcSize);
                     Environment.Exit(0);
             }
-            _data = new byte[(_pictH16>_pictH32?_pictH16:_pictH32)][][];
-            for (int i = 0; i < (_pictH16>_pictH32?_pictH16:_pictH32); i++)
+            _data = new byte[_realH][][];
+            for (int i = 0; i < _realH; i++)
             {
-                _data[i] = new byte[(_pictW16>_pictW32?_pictW16:_pictW32)][];
-                for (int j = 0; j < (_pictW16>_pictW32?_pictW16:_pictW32); j++)
+                _data[i] = new byte[_realW][];
+                for (int j = 0; j < _realW; j++)
                 {
                     _data[i][j] = br.ReadBytes(3);
                 }
-
-                if (((_pictW16>_pictW32?_pictW16:_pictW32) * 3) % 4 > 0) br.ReadBytes(4 - (((_pictW16>_pictW32?_pictW16:_pictW32) * 3) % 4));
+                int width = _realW*3;
+                while (width % 4 != 0)
+                {
+                    nullb = br.ReadByte();
+                    width++;
+                }
             }
             br.Close();
         }
@@ -64,44 +71,46 @@ namespace bmpTest
             bw.Write(_flSize);
             bw.Write(next8bites);
             bw.Write(bcSize);
-            bw.Write((_pictW16>_pictW32?_pictW16:_pictW32));
+            bw.Write(_pictW16>_pictW32?_pictW16:_pictW32);
             bw.Write((_pictH16>_pictH32?_pictH16:_pictH32));
             bw.Write(lastBites);
-            for (int i = 0; i < (_pictH16>_pictH32?_pictH16:_pictH32); i++)
+            for (int i = 0; i < _realH; i++)
             {
-                for (int j = 0; j < (_pictW16>_pictW32?_pictW16:_pictW32); j++)
+                for (int j = 0; j < _realW; j++)
                 {
                     bw.Write(_data[i][j]);
                 }
-
-                if (((_pictW16>_pictW32?_pictW16:_pictW32) * 3) % 4 > 0) for (int ctr = 0; ctr < 4 - (((_pictW16>_pictW32?_pictW16:_pictW32) * 3) % 4); ctr++) bw.Write(false);
+                int width = _realW*3;
+                while (width%4!=0) 
+                {
+                    bw.Write(nullb);
+                    width++;
+                }
             }
         }
         public void Enlarge(Int16 coef)
         {
-            byte[][][] nData = new byte[(_pictH16>_pictH32?_pictH16:_pictH32)*coef][][];
-            for (int i = 0; i < (_pictH16 > _pictH32 ? _pictH16 : _pictH32); i++)
+            byte[][][] nData = new byte[_realH*coef][][];
+            for (int i = 0; i < _realW; i++)
             {
                 for (int ctr = 0; ctr < coef; ctr++)
                 {
-                    nData[i*coef + ctr] = new byte[(_pictW16 > _pictW32 ? _pictW16 : _pictW32) * coef][];
+                    nData[i*coef + ctr] = new byte[_realW * coef][];
                     for (int j = 0; j < (_pictW16 > _pictW32 ? _pictW16 : _pictW32); j++)
                     {
                         for (int ctr2 = 0; ctr2 < coef; ctr2++)
                         {
-                            nData[i * coef + ctr][j * coef + ctr] = _data[i][j];
+                            nData[i * coef + ctr][j * coef + ctr2] = _data[i][j];
                         }
                     }
                 }
             }
-            _data = nData;
-            _pictH16 *= coef;
-            _pictH32 *= coef;
-            _pictW16 *= coef;
-            _pictW32 *= coef;
-            _flSize = 14 + bcSize + (_pictH16 > _pictH32
-                ? (_pictW16 * 3 % 4 == 0 ? _pictW16 * _pictH16 * 3 : (_pictW16 * 3 + 4 - _pictW16 * 3 % 4) * _pictH16)
-                : (_pictW32 * 3 % 4 == 0 ? _pictW32 * _pictH32 * 3 : (_pictW32 * 3 + 4 - _pictW32 * 3 % 4) * _pictH32));
+            _data = nData; 
+            _pictH16 *= coef; _pictH32 *= coef; _realH *= coef;
+            _pictW16 *= coef; _pictW32 *= coef; _realW *= coef;
+            _flSize = 14 + bcSize + (_realW % 4 == 0
+                ? _realW * _realH * 3
+                : (_realW * 3 + 4 - _realW * 3 % 4) * _realH);
         }
     }
 }
