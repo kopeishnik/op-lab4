8ac4f04e1ddae878d9cf04bae9aecd71bdcb07c4
Andrew <71765448+CherpakAndrii@users.noreply.github.com>
2021-04-20 15:48:53 +0300

    залив новіший мейн

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text;
 
@@ -9,15 +9,13 @@ namespace bmpTest
         private static string _path;
         private static string _flName;
         private static string _nFlName;
-        private static int szCoef = 1;
+        private static double _szCoef = 1;
         static void Main()
         {
-            // GetFromUser();
-            _path = Environment.CurrentDirectory;
-            _flName = @"\bmp.bmp";
-            _nFlName = @"\new.bmp";
-            //
+            GetFromUser();
             Pict inp = new Pict(_path + _flName);
+            if (_szCoef>1&&_szCoef%1==0) inp.Enlarge((short)_szCoef);
+            else if (_szCoef<1&&_szCoef%1==0)
             inp.ToFile(_path + _nFlName);
         }
 
@@ -26,10 +24,13 @@ namespace bmpTest
             Console.Write("Enter the filename: ");
             _path = Environment.CurrentDirectory;
             _flName = Console.ReadLine();
-            if (_flName.Length==0) _flName = @"\bmp.bmp";
+            if (_flName==null||_flName.Length<5) _flName = @"\bmp.bmp";
+            Console.Write("Enter the new filename: ");
+            _nFlName = Console.ReadLine();
+            if (_nFlName==null||_nFlName.Length<5) _nFlName = @"\new.bmp";
             Console.Write("Enter the size coefficient: ");
-            int n = Int32.Parse(Console.ReadLine());
-            if (n > 0) szCoef = n;
+            string n = Console.ReadLine();
+            if (n!=null&&Double.Parse(n)!=0) _szCoef = Double.Parse(n);
         }
     }
 }
